1b2c3d5f07275682d02b20842f2ca67283a2a5b6
Giuseppe Mercurio <giuseppemercurio1997@gmail.com>
2022-09-29 07:42:31 +0200

    completed Contact.js component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,30 @@
 import React from "react";
 
 export default function Contact() {
+
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function encode(data) {
+        return Object.keys(data)
+            .map(
+                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+            )
+            .join("&");
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/", {
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: encode({"form-name": "contact", name, email, message}),
+        })
+            .then(() => alert("Message sent!"))
+            .catch((error) => alert(error));
+    }
+
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex md:flex-nowrap flex-wrap">
@@ -46,6 +70,7 @@ export default function Contact() {
                 <form
                     netlify
                     name="contact"
+                    onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Hire Me
@@ -62,6 +87,7 @@ export default function Contact() {
                             type="text"
                             id="name"
                             name="name"
+                            onChange={(e) => setName(e.target.value)}
                             className="bg-gray-800 w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                         />
                     </div>
@@ -73,6 +99,7 @@ export default function Contact() {
                             type="email"
                             id="email"
                             name="email"
+                            onChange={(e) => setEmail(e.target.value)}
                             className="bg-gray-800 w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                         />
                     </div>
@@ -83,6 +110,7 @@ export default function Contact() {
                         <textarea
                             id="message"
                             name="message"
+                            onChange={(e) => setMessage(e.target.value)}
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                         />
                     </div>
